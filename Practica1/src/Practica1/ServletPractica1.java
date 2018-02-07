337b4d742b849b8a5350261c39e6d0a37e423bd9
antonio <antonio@10.82.86.198>
2018-02-07 18:39:58 +0100

    Obtención de las cabeceras de petición HTTP

--- Practica1/src/Practica1/ServletPractica1.java
+++ Practica1/src/Practica1/ServletPractica1.java
@@ -1,6 +1,9 @@
 package Practica1;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,28 +17,48 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletPractica1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    // Constructor
     public ServletPractica1() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
+    
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Dentro del Servlet hay dos métodos importantes: doGet y doPost. Estos métodos son
+	 * los que atienden las peticiones POST y GET de la página, respectivamente.
 	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		requestHeader(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	
+	//Creamos una clase que nos permita obtener las cabeceras de petición HTTP
+	public void requestHeader(HttpServletRequest req, HttpServletResponse res) throws IOException {
+				//
+		        PrintWriter out = res.getWriter();
+		        
+		        Enumeration<String> headerNames = req.getHeaderNames();
+		        while (headerNames.hasMoreElements()) {
+		            String headerName = headerNames.nextElement();
+
+		            out.write("\n"+headerName);
+		            Enumeration<String> headers = req.getHeaders(headerName);
+		            while (headers.hasMoreElements()) {
+	
+		                String headerValue = headers.nextElement();
+		                out.write(":"+headerValue);
+		            }
+
+		        }
+		        out.close();
+
+		    }
+		
 }
