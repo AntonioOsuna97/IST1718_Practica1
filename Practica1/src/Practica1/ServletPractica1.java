ab9dd040f179150fc2772c9d5375ca5ca28a0771
antonio <antonio@10.82.86.198>
2018-02-07 20:08:01 +0100

    Practica comentada sin la parte opcional

--- Practica1/src/Practica1/ServletPractica1.java
+++ Practica1/src/Practica1/ServletPractica1.java
@@ -42,22 +42,35 @@ public class ServletPractica1 extends HttpServlet {
 	
 	//Creamos un método que nos permita obtener las cabeceras de petición HTTP
 	public void requestHeader(HttpServletRequest req, HttpServletResponse res) throws IOException {
-				//
+				//Definimos una variable out de tipo Printwriter.
+				//Esta clase imprime representaciones formateadas de objetos en una secuencia de salida de texto.
 		        PrintWriter out = res.getWriter();
 		        
+		        //Interfaz Enumeration: Un objeto que implementa la interfaz Enumeration permite generar series de elementos
+		        //Enumeración llamada headerNames a la que le asignamos las distintas cabeceras sin su valor
 		        Enumeration<String> headerNames = req.getHeaderNames();
-		        while (headerNames.hasMoreElements()) {
+		        
+		        while (headerNames.hasMoreElements()) { //hasMoreElements()-> prueba si esta enumeración contiene más elementos
+		        	//Definimos una variable headerName de tipo String y le asignamos el valor siguiente de headerNames (contiene
+		        	//los nombres de las cabeceras)
 		            String headerName = headerNames.nextElement();
-
+		            //En nuestra salida, escribe un salto de línea + el nombre de la cabecera
 		            out.write("\n"+headerName);
+		            
+		            //Enumeracion de tipo string llamada header a la que le asignamos el valor de las distintas cabeceras
+		            //req.getHeaders(headerName) -> Obtiene el valor de las cabeceras de la petición
 		            Enumeration<String> headers = req.getHeaders(headerName);
-		            while (headers.hasMoreElements()) {
+		            
+		            while (headers.hasMoreElements()) { //prueba si esta enumeración contiene más elementos
 	
+		            	//Definimos una variable que contendra el valor de nuestra cabecera y se lo asignamos
 		                String headerValue = headers.nextElement();
+		                //Escribe en nuestra página web : y el valor de la cabecera
 		                out.write(":"+headerValue);
 		            }
 
 		        }
+		        //Cerramos salida.
 		        out.close();
 
 		    }
